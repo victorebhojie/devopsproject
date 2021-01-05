49ef984b7d44eb91b18e586adefbeff71d088473
victor ebhojie <victorebhojie@gmail.com>
2021-01-05 23:35:27 +0100

    new update for Tfvars

--- variables.tf
+++ variables.tf
@@ -25,3 +25,7 @@ variable "nic_names" {
 }
 
 
+variable "sub" { type = string }
+variable "client_secret" { type = string }
+variable "client_id" { type = string }
+variable "tenant_id" { type = string }
