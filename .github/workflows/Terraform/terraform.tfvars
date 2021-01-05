9bd89de7786b78ab9f09bdd29075b11bd3b60220
victor ebhojie <victorebhojie@gmail.com>
2021-01-05 06:46:02 +0100

    first commit

---
+++ .github/workflows/Terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+sub = 0be5258f-1755-4279-80ff-7485c444defb
+client_id = e59ffff8-2dcd-4adc-8a2a-2fbff15f5ced
+tenant_id = ae278fc0-4b73-48ef-90b1-063a57846873
+nic_names = ApacheNIC
+publicip = ApacheIP
+servers = ApacheVM
+rg = ApacheRG
+vnet = Apache-VNET
---
+++ .github/workflows/Terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "vnet" {
+  type    = string
+}
+variable "rg" {
+  type    = string
+}
+
+variable "servers" {
+  type    = string
+
+
+}
+
+
+variable "publicip" {
+  type    = string
+
+}
+
+variable "nic_names" {
+  type    = string
+}
+
+
+variable "sub" { type = string }
+variable "client_secret" { type = string }
+variable "client_id" { type = string }
+variable "tenant_id" { type = string }
