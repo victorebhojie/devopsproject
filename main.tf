0c21f1b4e739c4fa5c8122071ac1c832bc0bd50f
victor ebhojie <victorebhojie@gmail.com>
2021-01-06 00:23:01 +0100

    commit to fix auth issue

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ terraform {
 
 }
 provider "azurerm" {
-  version = "~>2.0"
+  version         = "~>2.0"
+  subscription_id = var.sub
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
   features {}
 }
 
